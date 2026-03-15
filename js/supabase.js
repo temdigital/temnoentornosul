043edfe16d329dcace266d808e58e940c952f678
temdigital <temnoentornosul@gmail.com>
2026-03-15 11:56:00 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-// configuração simples do Supabase
-
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = "https://flidbkfrfosuahgphiza.supabase.co"
-const supabaseKey = "sb_publishable_mCBYSpZc45Pw345wZ1coEA_rYL61Ea8"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
